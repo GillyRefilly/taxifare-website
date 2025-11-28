5234a5e5c43cc459aeffd8657ec5a12c14173742
Gillian Dornan <gillian.dornan@gmail.com>
2025-11-28 15:46:48 +0100

    update

--- app.py
+++ app.py
@@ -4,69 +4,61 @@ import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point, Polygon
 import requests
+from streamlit_extras.let_it_rain import rain
 
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 
 '''
-# TaxiFareModel front
+# :money_with_wings: Taxi Fare Predictinator :money_with_wings:
+
+#### Watch that hard earned cash fly out the window
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Where would you like to go?
-
-'''
 
 #date-time
-d = st.date_input(
+d = st.sidebar.date_input(
     "Select the date of the ride.",
     datetime.date(2019, 7, 6))
-t = st.time_input('Select the time of the ride', datetime.time(8, 45))
+t = st.sidebar.time_input('Select the time of the ride', datetime.time(8, 45))
 
 date = datetime.datetime.combine(d, t)
-st.write('Your selected date and time are:', date, 'cool')
 
 #pickup and dropoff locations
 
 st.sidebar.write("Pickup address")
-# street_p = st.sidebar.text_input("Street", "75 Bay Street")
-# city_p = st.sidebar.text_input("City", "Toronto")
-# province_p = st.sidebar.text_input("Province", "Ontario")
-# country_p = st.sidebar.text_input("Country", "Canada")
+street_p = st.sidebar.text_input("Street", "75 Bay Street")
+city_p = st.sidebar.text_input("City", "Toronto")
+province_p = st.sidebar.text_input("Province/State", "Ontario")
+country_p = st.sidebar.text_input("Country", "Canada")
 
-# geolocator = Nominatim(user_agent="GTA Lookup")
-# geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-# location = geolocator.geocode(street_p+", "+city_p+", "+province_p+", "+country_p)
+geolocator = Nominatim(user_agent="GTA Lookup",timeout=10)
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+location_p = geolocator.geocode(street_p+", "+city_p+", "+province_p+", "+country_p)
 
+lat_p = location_p.latitude
+lon_p = location_p.longitude
 
-lat_p = st.sidebar.number_input('Insert your pickup latitutde')
-lon_p = st.sidebar.number_input('Insert your pickup longitude')
-
-st.write('Your pickup location is ', lat_p, lon_p)
-
-st.sidebar.write("Pickup address")
-# street_d = st.sidebar.text_input("Street", "446 Keith Rd E")
-# city_d = st.sidebar.text_input("City", "North Vancouver")
-# province_d = st.sidebar.text_input("Province", "British Columbia")
-# country_d = st.sidebar.text_input("Country")
-
-# geolocator = Nominatim(user_agent="GTA Lookup")
-# geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-# location = geolocator.geocode(street_d+", "+city_d+", "+province_d+", "+country_d)
-
-# lat_d = location.latitude
-# lon_d = location.longitude
-lat_d = st.sidebar.number_input('Insert your dropoff latitutde')
-lon_d = st.sidebar.number_input('Insert your dropoff longitutdakjhsfkwh')
+# lat_p = st.sidebar.number_input('Insert your pickup latitutde')
+# lon_p = st.sidebar.number_input('Insert your pickup longitude')
 
 
-st.write('Your dropoff location is ', lat_d, lon_d)
+st.sidebar.write("Dropoff address")
+street_d = st.sidebar.text_input("Street","Rudi-Dutschke-Straße 26")
+city_d = st.sidebar.text_input("City","Berlin")
+province_d = st.sidebar.text_input("Province/State","Berlin")
+country_d = st.sidebar.text_input("Country","Germany")
+
+geolocator = Nominatim(user_agent="GTA Lookup",timeout=10)
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+location_d = geolocator.geocode(street_d+", "+city_d+", "+province_d+", "+country_d)
+
+lat_d = location_d.latitude
+lon_d = location_d.longitude
+# lat_d = st.sidebar.number_input('Insert your dropoff latitutde')
+# lon_d = st.sidebar.number_input('Insert your dropoff longitutdakjhsfkwh')
+
 
 map_data = pd.DataFrame({'lat': [lat_p,lat_d], 'lon': [lon_p,lon_d]})
 st.map(map_data)
@@ -76,33 +68,19 @@ st.map(map_data)
 number = st.slider('Number of passengers', min_value=1, max_value=12, value=5, step=1)
 
 st.write('You need transportation for ', number, 'people')
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+#Confirm selected params
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
+'''## Interesting choices you've just made there...'''
 
-url = 'https://taxifare.lewagon.ai/predict'
-
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+st.write('Your selected date and time are:', date)
+st.write('Your pickup address translates to:', lat_p,lon_p)
+st.write('Your pickup address translates to:', lat_d,lon_d)
+st.write('Your passenger number is:', number)
 
 
 url = 'https://taxifare.lewagon.ai/predict'
+
 
 params = {
     'pickup_datetime': date, # 0 for Sunday, 1 for Monday, ...
@@ -117,18 +95,22 @@ response = requests.get(url, params=params).json()
 fare = response['fare']
 
 if fare > 100:
-    exclamation = 'Oooff.. expensive'
+    exclamation = 'Mr. Moneybags'
+    rain(
+        emoji="💸",
+        font_size=54,
+        falling_speed=5,
+        animation_length="infinite",
+    )
 elif fare > 20:
-    exclamation = 'Gotta pay to get somewhere...'
+    exclamation = 'Naja...Sometimes you just really need a lazy bolt'
 else:
-    exclamation = 'Good Deal!'
+    exclamation = 'Sick Deal!'
 f'''
 # {exclamation}
 Based on our model calulation your fare will be
-# {fare}
+# {round(float(fare),2):.2f} buckaroos
 '''
 
 if st.button('More 🎈🎈🎈 please!'):
     st.balloons()
-
-# st.write('Predicted fare ', response.json(), 'buckaroos')
